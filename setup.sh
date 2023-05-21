fe91e499ef2b4e711ab9a5bcee6fa637d224ab9d
Suyash Gautam <suyash.gautam97@gmail.com>
2023-05-21 03:41:08 +0000

    Some fixes

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ sh ./src/server/migrations/import_metadata.sh
 
 pip install -r requirements.txt
 
-sudo apt-get install libgraphviz-dev 
+sudo apt-get install python3-dev libgraphviz-dev 
 pip install -r src/server/sql_graph/requirements.txt
