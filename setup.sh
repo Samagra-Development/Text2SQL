5f8080113fa33749eec665093d56bf7dfdebad77
Pramod Goyal <81946962+goyalpramod@users.noreply.github.com>
2023-07-22 16:59:10 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 sh ./src/server/migrations/import_metadata.sh
 
 sudo apt-get update
-sudo pip install -r requirements.txt
+sudo apt-get install python3-venv
 
-sudo apt-get install python3-dev libgraphviz-dev 
-sudo pip install -r src/server/sql_graph/requirements.txt
+# Create and activate a virtual environment (without sudo)
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies inside the virtual environment (without sudo)
+pip3 install -r requirements.txt
+
+# Additional installations (without sudo)
+sudo apt-get install python3-dev libgraphviz-dev
+pip3 install -r src/server/sql_graph/requirements.txt
+python3 src/server/app.py
